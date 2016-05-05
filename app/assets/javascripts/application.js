819c15b087e508f38d719ed90dbf41c687270112
Hong, Chang Nam <hcn1519@gmail.com>
2016-05-05 09:39:13 +0000

    viewset

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery-ui
+//= require jquery.mobile
 //= require_tree .
 //= require bootstrap
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
+  root 'home#index'
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
