5ba3e9920814b2181da8975d6427d2ea297da3c7
Hong, Chang Nam <hcn1519@gmail.com>
2016-05-11 09:57:25 +0000

    merge

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -8,30 +8,40 @@ $(document).ready(function(){
   var box4 = $(".paginate").height();
   var box5 = $(".fb-like").height();
   
+  var box6 = $("#detail-title").height();
+  var box7 = $(".map-icon").height();
+  
   var window_height = $(window).height(); 
   var container_box_height = $('.container-box').height();
   
   var bottom_padding_window_short = window_height - container_box_height;
-  var bottom_padding_window_long = (box1 + box2 + box3 + box4 + box5) - container_box_height + footer_height;
-  
+  var bottom_padding_window_long1 = (box1 + box2 + box3 + box4 + box5 ) - container_box_height + footer_height;
+  var bottom_padding_window_long2 = (box2 + box3 + box6 + box7 + 50 ) - container_box_height + footer_height;
   
   var window_width = $(window).width();
   
+    //  내용이 윈도우 한페이지에 들어갈 때
      if( window_height > container_box_height) {
-      //웹에서 늘리기
+      
+        // 웹
         if ( window_width  > 600 ) {
             $(".content-box").css("padding-bottom", bottom_padding_window_short);  
-        } else {
-            $(".content-box").css("padding-bottom", bottom_padding_window_long+60);  
+        } 
+        // 모바일
+        else {
+            $(".content-box").css("padding-bottom", bottom_padding_window_long2);  
         }
-     } else {
-       if( window_width  > 600) {
+        
+     }
+    //  내용이 윈도우 한페이지에 안 들어갈 때
+     else {
         //웹용
-         $(".content-box").css("padding-bottom", bottom_padding_window_long);   
+       if( window_width  > 600) {
+         $(".content-box").css("padding-bottom", bottom_padding_window_long1 + 10);   
        }
+       //모바일용
        else { 
-           //모바일용
-         $(".content-box").css("padding-bottom", bottom_padding_window_long+30);   
+         $(".content-box").css("padding-bottom", bottom_padding_window_long1 + 30);   
        }
        
      }
